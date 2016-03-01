6ef4509ac4bad4af724f9c0150c560e6ac948fb1
Charles Mailly <charles.mailly@free.fr>
2016-03-01 02:20:05 +0100

    New compile -> move dir

--- bin/AndroidManifest.xml.d
+++ bin/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-D:\Android\Projects\TestAharos\bin\AndroidManifest.xml : \
-D:\Android\Projects\TestAharos\AndroidManifest.xml \
+D:\GitHub\TestNodeEngine\bin\AndroidManifest.xml : \
+D:\GitHub\TestNodeEngine\AndroidManifest.xml \
--- bin/NativeActivity-debug-unaligned.apk.d
+++ bin/NativeActivity-debug-unaligned.apk.d
@@ -1,11 +1,11 @@
-D:\Android\Projects\TestAharos\bin\NativeActivity-debug-unaligned.apk : \
-D:\Android\Projects\TestAharos\bin\NativeActivity.ap_ \
-D:\Android\Projects\TestAharos\libs\armeabi-v7a\libc++_shared.so \
-D:\Android\Projects\TestAharos\libs\armeabi-v7a\libopenal.so \
-D:\Android\Projects\TestAharos\libs\armeabi-v7a\libsfml-activity.so \
-D:\Android\Projects\TestAharos\libs\armeabi-v7a\libsfml-audio.so \
-D:\Android\Projects\TestAharos\libs\armeabi-v7a\libsfml-example.so \
-D:\Android\Projects\TestAharos\libs\armeabi-v7a\libsfml-graphics.so \
-D:\Android\Projects\TestAharos\libs\armeabi-v7a\libsfml-network.so \
-D:\Android\Projects\TestAharos\libs\armeabi-v7a\libsfml-system.so \
-D:\Android\Projects\TestAharos\libs\armeabi-v7a\libsfml-window.so \
+D:\GitHub\TestNodeEngine\bin\NativeActivity-debug-unaligned.apk : \
+D:\GitHub\TestNodeEngine\bin\NativeActivity.ap_ \
+D:\GitHub\TestNodeEngine\libs\armeabi-v7a\libc++_shared.so \
+D:\GitHub\TestNodeEngine\libs\armeabi-v7a\libopenal.so \
+D:\GitHub\TestNodeEngine\libs\armeabi-v7a\libsfml-activity.so \
+D:\GitHub\TestNodeEngine\libs\armeabi-v7a\libsfml-audio.so \
+D:\GitHub\TestNodeEngine\libs\armeabi-v7a\libsfml-example.so \
+D:\GitHub\TestNodeEngine\libs\armeabi-v7a\libsfml-graphics.so \
+D:\GitHub\TestNodeEngine\libs\armeabi-v7a\libsfml-network.so \
+D:\GitHub\TestNodeEngine\libs\armeabi-v7a\libsfml-system.so \
+D:\GitHub\TestNodeEngine\libs\armeabi-v7a\libsfml-window.so \
